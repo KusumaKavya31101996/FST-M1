eeb3113cd1f938413e399788f3a88643560b2f54
KusumaKavya31101996 <151725925+KusumaKavya31101996@users.noreply.github.com>
2024-06-28 13:42:18 +0530

    Delete Java/Activities/NotEnoughFundExecption.java

--- Java/Activities/NotEnoughFundExecption.java
+++
@@ -1,9 +0,0 @@
-package Activities;
-
-public class NotEnoughFundExecption extends RuntimeException {
-
-    public NotEnoughFundExecption(Integer amount, Integer balance) {
-        super("Attempted to withdraw " + amount + " with a balance of " + balance);
-    }
-
-}
